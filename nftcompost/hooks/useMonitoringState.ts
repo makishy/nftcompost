c04b438f88f9a20bc0a5823e1eaf5368561b17de
makishy <makishy.dev@gmail.com>
2022-11-03 11:54:28 +0900

    real-time update of soil points

--- nftcompost/hooks/useMonitoringState.ts
+++ nftcompost/hooks/useMonitoringState.ts
@@ -5,6 +5,8 @@ import {
   collection,
   doc,
   getDocs,
+  onSnapshot,
+  query,
 } from 'firebase/firestore'
 export const useDeviceMonitoringState = (deviceId?: string) => {
   const [monitoringStates, setMonitoringStates] = useState<MonitoringState[]>([])
@@ -13,14 +15,24 @@ export const useDeviceMonitoringState = (deviceId?: string) => {
       if (deviceId) {
         const docRef = doc(db, 'devices', deviceId)
         const pointRef = collection(docRef, 'point')
-        const snap = await getDocs(pointRef)
-        snap.forEach((doc) => {
-          setMonitoringStates([MonitoringState.fromData(doc.data())])
-          console.log(doc.id, doc.data())
+        const q = query(pointRef)
+        const unsubscribe = onSnapshot(q, (snap) => {
+          snap.docChanges().forEach((change) => {
+            console.log('change', change)
+            if (change.type === 'added' || change.type === 'modified') {
+              console.log('change.doc', change.doc)
+              const data = change.doc.data()
+              console.log('data', data)
+              setMonitoringStates([MonitoringState.fromData(data)])
+            }
+          })
         })
+        return () => unsubscribe()
       }
     }
     f()
   }, [deviceId])
+
+
   return { monitoringStates }
 }
